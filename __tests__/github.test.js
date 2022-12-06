9cc8f66d686c061dc1f6437734fb74b35b6333d7
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-12-06 08:53:09 -0800

    add github mocks for testing

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+// const UserService = require('../lib/services/UserService');
 
 jest.mock('../lib/services/github');
 
@@ -10,17 +10,30 @@ describe('GitHub auth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  afterAll(() => {
-    pool.end();
-  });
 
-  it('/api/v1/github/login should redirect to the github OAuth page', async () => {
+  it('/api/v1/github/login should redirect to github OAuth page', async () => {
     const res = await request(app).get('/api/v1/github/login');
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
   });
 
+  it('/api/v1/github/callback should login users and redirect to dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      login: 'fictitious',
+      email: 'test@useremail.net',
+      avatar: 'http://placekitten.com/200/300',
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
 
+  afterAll(() => {
+    pool.end();
+  });
 });
-
