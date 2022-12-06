0ae9c87b89a5244bdea8f790da09b690e884d6b5
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-12-06 13:55:21 -0800

    add basic UI, tests passing

--- sql/setup.sql
+++ sql/setup.sql
@@ -23,5 +23,15 @@ CREATE TABLE github_users (
 
 CREATE TABLE posts (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  post VARCHAR(255)
+  postbody VARCHAR(255)
 );
+
+INSERT INTO posts (postbody)
+VALUES
+('here is a post about posts'),
+('here is another post, wonder what it is about?'),
+('I do not yet know how to escape single quotes in SQL'),
+('perhaps the answer I seek has been within me the entire time'),
+('perchance perhaps peradventure possibly maybe mayhaps conceivably feasibly'),
+('the greatest treasure is the friends we made along the way'),
+('but also mostly the treasure is the treasure tbh');
