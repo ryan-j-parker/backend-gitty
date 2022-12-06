c0444e36d230e40245d43738e616c4f984ad4dfe
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-12-05 17:05:47 -0800

    add test for login route, set up sql

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
+jest.mock('../lib/services/github');
+
 describe('GitHub auth routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -14,7 +16,11 @@ describe('GitHub auth routes', () => {
 
   it('/api/v1/github/login should redirect to the github OAuth page', async () => {
     const res = await request(app).get('/api/v1/github/login');
-    expect(res.header.location).toMatch();
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
+
+
 });
 
